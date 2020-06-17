6273ab79de9c7743fcec1642c401f6a95aac2c4d
Bernhard Liebl <poke1024@gmx.de>
2020-06-17 08:57:38 +0200

    better line skew estimation algorithm

--- origami/core/dewarp.py
+++ origami/core/dewarp.py
@@ -1,4 +1,5 @@
 import skimage.filters
+import skimage.morphology
 import numpy as np
 import PIL.Image
 import math
@@ -6,10 +7,17 @@ import cv2
 import scipy
 
 
+def make_ellipse(w, h):
+	structure = skimage.morphology.disk(max(w, h)).astype(np.float32)
+	structure = cv2.resize(structure, (w, h), interpolation=cv2.INTER_AREA)
+	return structure / np.sum(structure)
+
+
 class LineSkewEstimator:
-	def __init__(self):
+	def __init__(self, min_length=50, kernel=(16, 8)):
 		self._max_phi = 30 * (math.pi / 180)
-		# put more rule-based parameters here.
+		self._min_length = min_length
+		self._kernel = kernel
 
 	def _binarize(self, im):
 		im = im.convert("L")
@@ -21,6 +29,8 @@ class LineSkewEstimator:
 	def _detect_line_regions(self, im):
 		pix2 = self._binarize(im)
 
+		# old algorithm (more parameters).
+		"""
 		pix2 = scipy.ndimage.morphology.binary_dilation(
 			pix2, np.ones((1, 2)), iterations=2)
 
@@ -32,6 +42,21 @@ class LineSkewEstimator:
 
 		pix2 = scipy.ndimage.morphology.binary_opening(
 			pix2, np.ones((5, 5)), iterations=1)
+		"""
+
+		pix2 = skimage.filters.sobel_h(pix2)
+		pix2 = (pix2 == 1).astype(np.uint8) * 255
+
+		pix2 = skimage.filters.sobel_h(pix2)
+		pix2 = (pix2 == 1).astype(np.uint8) * 255
+
+		pix2 = scipy.ndimage.filters.convolve(
+			pix2.astype(np.float32) / 255, make_ellipse(*self._kernel))
+
+		thresh = skimage.filters.threshold_sauvola(pix2, 3)
+		pix2 = (pix2 > thresh).astype(np.uint8) * 255
+
+		pix2 = np.logical_not(pix2)
 
 		return pix2
 
@@ -42,9 +67,11 @@ class LineSkewEstimator:
 		props = skimage.measure.regionprops(pix3)
 
 		for prop in props:
-			if prop.major_axis_length < 100:  # not enough evidence
+			if prop.major_axis_length < self._min_length:
+				# not enough evidence
 				continue
-			if prop.eccentricity < 0.98:  # not line-shaped enough
+			if prop.eccentricity < 0.99:
+				# not line-shaped enough
 				continue
 
 			p = prop.centroid
@@ -65,7 +92,14 @@ class LineSkewEstimator:
 		for p, phi in self(im):
 			p = tuple(map(int, p))[::-1]
 			r = 50
-			q = np.array(p) + np.array([math.cos(phi), math.sin(phi)]) * r
+			v = np.array([math.cos(phi), math.sin(phi)]) * r
+
+			q = np.array(p) + v
 			q = tuple(map(int, q))
 			cv2.line(pixels, p, q, (255, 0, 0), 2)
+
+			q = np.array(p) - v
+			q = tuple(map(int, q))
+			cv2.line(pixels, p, q, (255, 0, 0), 2)
+
 		return PIL.Image.fromarray(pixels)
