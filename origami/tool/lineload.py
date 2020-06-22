af22bd4bb05e6ee79f7cabeb4f177b8244ed9ae1
Bernhard Liebl <poke1024@gmx.de>
2020-06-22 17:09:05 +0200

    fixes to work with lastest dewarping changes

--- origami/tool/lineload.py
+++ origami/tool/lineload.py
@@ -9,16 +9,19 @@ from origami.core.block import Block, Line
 
 
 class LineLoader:
+	def __init__(self, dewarped=False):
+		self._dewarped = dewarped
+
 	@lru_cache(maxsize=10)
 	def _load_page(self, full_page_path):
-		return Page(full_page_path)
+		return Page(full_page_path, dewarp=self._dewarped)
 
 	@lru_cache(maxsize=10)
 	def _load_block(self, full_page_path, block_path):
 		with zipfile.ZipFile(full_page_path.with_suffix(".contours.zip"), "r") as zf:
 			name = block_path + ".wkt"
 			polygon = shapely.wkt.loads(zf.read(name).decode("utf8"))
-			return Block(self._load_page(full_page_path), polygon)
+			return Block(self._load_page(full_page_path), polygon, dewarped=self._dewarped)
 
 	def load_line(self, page_path, line_path):
 		block = self._load_block(
@@ -30,4 +33,4 @@ class LineLoader:
 		return Line(block, **line_info)
 
 	def load_line_image(self, page_path, line_path, **kwargs):
-		return self.load_line(page_path, line_path).image(**kwargs)
+		return self.load_line(page_path, line_path).image(dewarped=self._dewarped, **kwargs)
