35f1d5f12e5b40b312a5e43be05f0ef774e2ef37
Bernhard Liebl <poke1024@gmx.de>
2020-07-02 13:21:31 +0200

    basic implementation if batch.detect.stats

--- origami/batch/detect/stats.py
+++ origami/batch/detect/stats.py
@@ -1,9 +1,12 @@
 import imghdr
 import click
+import collections
 
 from pathlib import Path
+from tabulate import tabulate
 
 from origami.batch.core.processor import Processor
+from origami.batch.core.io import find_data_path
 
 
 class StatsProcessor(Processor):
@@ -17,16 +20,38 @@ class StatsProcessor(Processor):
 		else:
 			self._names = None
 
+		self._num_pages = 0
+		self._artifacts = collections.defaultdict(int)
+
+	def artifacts(self):
+		return []
+
 	def should_process(self, p: Path) -> bool:
-		return imghdr.what(p) is not None
+		return True
 
 	def process(self, page_path: Path):
 		if self._list_names:
-			self._names.append(page_path)
+			self._names.append(page_path.name)
 
-	@property
-	def names(self):
-		return self._names
+		self._num_pages += 1
+
+		data_path = find_data_path(page_path)
+		for p in data_path.iterdir():
+			if not p.name.startswith("."):
+				self._artifacts[p.name] += 1
+
+	def print(self):
+		data = []
+
+		data.append(["pages", str(self._num_pages)])
+		for name, n in sorted(self._artifacts.items()):
+			data.append([name, str(n)])
+
+		print(tabulate(data, tablefmt="psql"))
+
+		if self._list_names:
+			for name in self._names:
+				print(name)
 
 
 @click.command()
@@ -48,10 +73,7 @@ def stats(data_path, **kwargs):
 	""" List stats of pages in DATA_PATH. """
 	processor = StatsProcessor(kwargs)
 	processor.traverse(data_path)
-	if processor.names is not None:
-		print("found %d page(s):" % len(processor.names))
-		for name in processor.names:
-			print(name)
+	processor.print()
 
 
 if __name__ == "__main__":
