c5252599c77521150b5d9097f9e62940e2b26691
Bernhard Liebl <poke1024@gmx.de>
2022-01-18 11:15:23 +0100

    added quickstart examples for export and stats

--- quickstart/06_cpu_stats.sh
+++ quickstart/06_cpu_stats.sh
@@ -8,5 +8,5 @@ export ORIGAMI_WORK_DIR="/path/to/origami"  # !! adapt to your needs
 export DATA_PATH="$ORIGAMI_WORK_DIR/data"
 
 cd "$ORIGAMI_WORK_DIR/origami"
-python -m origami.batch.detect.compose --page-xml "$DATA_PATH"
-python -m origami.batch.utils.sample --all -a compose -o "$ORIGAMI_WORK_DIR/texts.zip" "$DATA_PATH"
+
+python -m origami.batch.utils.stats "$DATA_PATH"
