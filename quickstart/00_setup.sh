a5ea3371b3dbc16b2b7a614dc3815626d6932f41
Bernhard Liebl <poke1024@gmx.de>
2023-01-16 15:10:16 +0100

    fix dropbox download links

--- quickstart/00_setup.sh
+++ quickstart/00_setup.sh
@@ -6,12 +6,12 @@ git clone https://github.com/poke1024/origami
 
 # download and extract segmentation models from https://www.dropbox.com/home/Forschungsdaten/Permanent/origami/models/segmentation/origami-models-v3
 
-wget -O models.zip "https://uca58c50515cbfaf0363bb0cb6fe.dl.dropboxusercontent.com/zip_download_get/BBVtLmY7hWNyW_qEJe5Xp9kUgjz6oexJK4mMqZNsCWPBrIXJhpKmNHlI6dmSoB6tNd9cIrN4Cf2fFAMzRysSFEvJqQclDwwJPk7DzgWYACABJQ?_download_id=533591005431019280218829531675511048890558876600740525990494341846&_notify_domain=www.dropbox.com"
+wget -O models.zip https://uc3e52a80f466ae06a2aeaf6177d.dl.dropboxusercontent.com/zip_download_get/BYHm83cJuAkPoSHaOaEuP0Nklj-R2NSiS0s0UuzhOmnmWRZlPVex9F1EonUfHoQQ_WMfqyRnAsiylZcBja91wxwd-ssvBQhSnrfyFkWbjcKaEQ?_download_id=39650021329642565662846846334081210177730464747868812499130017443&_notify_domain=www.dropbox.com
 
 7z x models.zip
 
 # download ocr models from https://www.dropbox.com/home/Forschungsdaten/Permanent/origami/models/ocr/origami-v19-h56-styled-v2
 
-wget -O ocr_models.zip https://uc2a86067b2188b835119f164df1.dl.dropboxusercontent.com/zip_download_get/BBXU63ZitR4mvhoJL4RgKf8EXg2lTZurhYXigmytEVdkxmOkDqFtm3GkeK7-jJoDRiOCwvO9Hyzs6OuWXW4lTv6fGzPCwjdKyBWDI0FhHbxW7w?_download_id=9274836462678285607588886091311159303633115785869975384670749405&_notify_domain=www.dropbox.com
+wget -O ocr_models.zip https://uce885e6fe454abc401860d0ab47.dl.dropboxusercontent.com/zip_download_get/BYFm2I3jFmm9D1rKK26rJSFbLKCYRGHlq464PjAfBKyfRB28ygPsexKwei4kjKA9wEa6BCJEx9OPkrQq1S5SMOu5gn5jWqjQ1QCizk-7cYBE6Q?_download_id=51690070418615855778796283804879446316078219205726683540385665394&_notify_domain=www.dropbox.com
 
 7z x ocr_models.zip
